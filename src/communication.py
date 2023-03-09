2ca53b8054eb4b89c4562a637bace01bb9a0e4be
Dominik <dominik.ziegenhagel@gmail.com>
2023-03-09 11:30:53 +0100

    move code from communicaztion helper into communication.py

--- src/communication.py
+++ src/communication.py
@@ -3,13 +3,21 @@
 # Attention: Do not import the ev3dev.ev3 module in this file
 import json
 import ssl
+import paho.mqtt.client as mqtt
 
 """
 We're using MQTT auf QoS Level 2 (exactly once) to communicate with the server.
 """
 
-
 class Communication:
+
+    # setup secrets
+    group_id = '046'
+    password = 'PwQ3lFHkEl'
+
+    # setup MQTT client
+    client = mqtt.Client(client_id=group_id, clean_session=False, protocol=mqtt.MQTTv31)
+
     """
     Class to hold the MQTT client communication
     Feel free to add functions and update the constructor to satisfy your requirements and
@@ -23,20 +31,23 @@ class Communication:
         :param mqtt_client: paho.mqtt.client.Client
         :param logger: logging.Logger
         """
-        # DO NOT CHANGE THE SETUP HERE
-        self.client = mqtt_client
+
+        self.client.on_message = self.safe_on_message  # Assign pre-defined callback function to MQTT client
         self.client.tls_set(tls_version=ssl.PROTOCOL_TLS)
-        self.client.on_message = self.safe_on_message_handler
-        # Add your client setup here
-        self.client.connect('robolab.inf.h-brs.de', port=8883)
+        self.client.username_pw_set(self.group_id, password=self.password)  # Your group credentials, see the python skill-test for your group password
+        self.client.connect('mothership.inf.tu-dresden.de', port=8883)
 
-        # config
-        group_id = '046'
-        password = 'PwQ3lFHkEl'
+        # inital subscribe
+        self.client.subscribe('explorer/{}'.format(self.group_id), qos=2)  # Subscribe to topic explorer/xxx
+        self.client.loop_start()
 
-
-
+        # save logger
         self.logger = logger
+
+    # destructor
+    def __del__(self):
+        self.client.loop_stop()
+        self.client.disconnect()
 
     # DO NOT EDIT THE METHOD SIGNATURE
     def on_message(self, client, data, message):
@@ -67,8 +78,8 @@ class Communication:
         self.logger.debug('Send to: ' + topic)
         self.logger.debug(json.dumps(message, indent=2))
 
-        # YOUR CODE FOLLOWS (remove pass, please!)
-        pass
+        # send message
+        self.client.publish('explorer/{}'.format(self.group_id), payload=message, qos=2)
 
     # DO NOT EDIT THE METHOD SIGNATURE OR BODY
     #
@@ -82,9 +93,16 @@ class Communication:
         :param message: Object
         :return: void
         """
+
+        # debug info
+        print('Got message with topic "{}":'.format(message.topic))
+
         try:
             self.on_message(client, data, message)
         except:
             import traceback
             traceback.print_exc()
             raise
+
+
+print("communication.py loaded")
