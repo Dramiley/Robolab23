148a551b7d42e21cad22b545fa9ef22de6a764ed
Dominik <dominik.ziegenhagel@gmail.com>
2023-03-10 10:03:04 +0100

    change communication facade to reduce params to expected only

--- src/communication/communication_logger.py
+++ src/communication/communication_logger.py
@@ -5,8 +5,19 @@ class CommunicationLogger:
     Dummy logger class to replace the logger from the server
     """
 
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
     def debug(self, message):
-        print("==> CommunicationLog: " + message)
+        print(self.bcolors.OKCYAN + "==> CommunicationLog: " + message)
 
     def error(self, message):
-        print("==> CommunicationError: " + message)
+        print(self.bcolors.WARNING + "==> CommunicationError: " + message)
