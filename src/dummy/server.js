2ae47ba60c63f67091b4c23f718ff699427fbf96
Dominik <dominik.ziegenhagel@gmail.com>
2023-03-15 19:05:01 +0100

    :wrench: remove positions

--- src/dummy/server.js
+++ src/dummy/server.js
@@ -3,9 +3,7 @@ const app = express()
 const port = 3000
 const fs = require('fs');
 
-app.get('/position', (req, res) => {
-    res.send(fs.readFileSync('./position.json', 'utf8'));
-});
+
 app.get('/history', (req, res) => {
     res.send(fs.readFileSync('./history.json', 'utf8'));
 });
