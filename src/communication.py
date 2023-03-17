0e77cbff76c20c981de4db1f568c0103c4675b1f
Dominik <dominik.ziegenhagel@gmail.com>
2023-03-17 15:43:50 +0100

    reduce communication to base

--- src/communication.py
+++ src/communication.py
@@ -10,6 +10,7 @@ import time
 # import communication_facade
 from communication_facade import CommunicationFacade
 
+
 class Communication:
     # setup secrets
     group_id = '046'
@@ -59,12 +60,8 @@ class Communication:
         self.client.subscribe('comtest/{}'.format(self.group_id), qos=2)  # Subscribe to topic explorer/xxx
         self.client.loop_start()
 
-        # save logger
-        self.logger = logger
-
         # create facade
         self.facade = CommunicationFacade(self)
-
 
     # destructor
     def __del__(self):
@@ -73,12 +70,6 @@ class Communication:
         """
         self.client.loop_stop()
         self.client.disconnect()
-
-    def __setattr__(self, attr, value):
-        if attr == 'facade' and self.__dict__.get('facade', None) is not None:
-            self.logger.warning('Facade is already set. Refusing to overwrite it.')
-
-        self.__dict__[attr] = value
 
     def __publish(self, topic, payload: str, qos=2):
 
@@ -93,7 +84,7 @@ class Communication:
         try:
             payload_dict = json.loads(payload)
         except:
-            self.logger.debug("json.loads(payload) failed")
+            raise ValueError('payload is not a valid json string')
 
         # if the payload has the key startDirection and the value is None, raise an error
         if 'startDirection' in payload_dict and payload_dict['startDirection'] is None:
@@ -119,54 +110,22 @@ class Communication:
         client_id = client._client_id.decode('utf-8')
 
         if client_id != data:
-            self.logger.error('Client id does not match data')
             return
-        else:
-            self.logger.debug('Client id matches data')
 
         payload = ""
         try:
             if message.payload is not None:
                 payload = json.loads(message.payload.decode('utf-8'))
-            else:
-                self.logger.error('no payload')
         except:
-            self.logger.error("json.loads failed")
-
-        # if type notice, just print the message but do not handle it
-        types_notice = ['notice', 'syntax', 'adjust']
-        if 'type' in payload and payload['type'] in types_notice:
-            if 'message' in payload['payload']:
-                self.logger.info(payload['payload']['message'])
-            else:
-                self.logger.info(str(payload['payload']))
-            return
-
-        # if "from" is the client itself, ignore the message
-        if 'from' in payload and payload['from'] == "debug":
-            self.logger.error(str(payload['payload']))
-            return
-
-        # log on_message event, please dont remove this line, its probably the most useful debug line
-        self.logger.info(message.payload.decode('utf-8'))
+            raise ValueError('payload is not a valid json string')
 
         # if "from" is the client itself, ignore the message
         if 'from' in payload and payload['from'] == "client":
             return
 
-        # if "from" is debug and the message type is syntax, check if the syntax is correct
-        if 'from' in payload and payload['from'] == "debug" and payload['type'] == "syntax":
-            if payload['payload']['message'] == 'Incorrect':
-                self.logger.info("Syntax check errors: " + str(str(payload['payload']['errors'])))
-            return
-
-        # log message
-        # self.logger.debug(json.dumps(payload, indent=2))
-
         # check if message type is set
         if 'type' not in payload:
-            self.logger.error('Message type is not set')
-            return
+            raise ValueError('Message type not set')
 
         # if the type is planet, we need to subscribe to the planet channel
         if payload['type'] == 'planet':
@@ -174,16 +133,10 @@ class Communication:
             self.planet_name = payload['payload']['planetName']
             # subscribe to planet channel
             self.client.subscribe('planet/{}/{}'.format(self.planet_name, self.group_id), qos=2)
-            self.logger.success('Subscribed to planet/{}/{}'.format(self.planet_name, self.group_id))
 
         # check if message type has a callback registered and call it
         if payload['type'] in self.callbacks:
             self.callback(payload['type'], payload['payload'])
-        else:
-            self.logger.error('No callback for message type ' + payload['type'] + ' registered')
-
-    # In order to keep the logging working you must provide a topic string and
-    # an already encoded JSON-Object as message.
 
     def send_planet_message(self, topic, payload):
         """
@@ -194,11 +147,9 @@ class Communication:
         """
         # we must have a planet name
         if self.planet_name is None:
-            self.logger.error('No planet name set')
-            return
+            raise ValueError('No planet name set')
 
         # send message
-        self.check_syntax(topic, payload)
         self.__publish('planet/{}/{}'.format(self.planet_name, self.group_id), payload=payload, qos=2)
 
     def send_explorer_message(self, topic, payload):
@@ -210,7 +161,6 @@ class Communication:
         """
 
         # send message
-        self.check_syntax(topic, payload)
         self.__publish('explorer/{}'.format(self.group_id), payload=payload, qos=2)
 
     def send_message(self, topic, message):
@@ -232,7 +182,7 @@ class Communication:
         except:
             import traceback
             traceback.print_exc()
-            raise
+            raise ValueError('Error in on_message handler')
 
     def set_callback(self, message_type, callback):
         self.callbacks[message_type] = callback
@@ -249,25 +199,20 @@ class Communication:
         if message_type in payload_definitions:
             # check if payload is valid
             if not self.validate_payload(payload, payload_definitions[message_type]):
-                self.logger.error('Payload is not valid')
-                return
+                raise ValueError('Payload is not valid')
 
         # call callback
         # check if callback function signature matches the payload definition
         if len(self.callbacks[message_type].__code__.co_varnames) == len(payload) or \
                 len(self.callbacks[message_type].__code__.co_varnames) == len(payload) + 1:
-            # self.logger.success('Calling callback for message type ' + message_type)
-            # self.logger.debug('with payload: ' + str(payload))
-            # self.logger.debug('Required arguments: ' + str(self.callbacks[message_type].__code__.co_varnames))
-            # self.logger.debug('Provided arguments: ' + str(payload))
             self.callbacks[message_type](**payload)
         else:
-            self.logger.error('Callback function signature for "' + message_type + '" does not match payload '
-                                                                                   'definition. Has ' + str(len(
+            raise ValueError('Callback function signature for "' + message_type + '" does not match payload '
+                                                                                  'definition. Has ' + str(len(
                 self.callbacks[message_type].__code__.co_varnames)) + ' arguments (' + str(self.callbacks[
                                                                                                message_type].__code__.co_varnames) + '), but payload has ' + str(
                 len(payload)) + ' arguments.' +
-                              '\r\nPayload: ' + str(payload))
+                             '\r\nPayload: ' + str(payload))
 
     def validate_payload(self, payload, payload_definition):
 
@@ -276,27 +221,19 @@ class Communication:
         # check if all keys are set
         for key in payload_definition:
             if key not in payload:
-                self.logger.error('Key ' + key + ' is missing')
-                return False
+                raise ValueError('Key ' + key + ' is not set')
 
         # check if all keys are valid
         for key in payload:
             if key not in payload_definition:
-                self.logger.warning('Key ' + key + ' is not expected')
                 unexpected_keys.append(key)
-                # return False
 
         # delete all keys that are not expected so that the signature matches
         for key in unexpected_keys:
-            self.logger.warning('Deleting key ' + key)
             del payload[key]
 
         # payload is valid
         return True
-
-    def check_syntax(self, topic, payload):
-        # self.__publish('comtest/{}'.format(self.group_id), payload=payload, qos=2)
-        pass
 
     def done(self):
         print("Disconnecting from broker")
@@ -311,23 +248,11 @@ class Communication:
         # reset counter
         self.received_since_last_path_select = 0
 
-        print("received_since_last_path_select: " + str(self.received_since_last_path_select))
-
-        # send message
-        print("waiting 3s before faking server response")
-
-        time.sleep(3.2)  # TODO: change to 3s
-
-        print("waited 3s before faking server response")
-
-        print("received_since_last_path_select: " + str(self.received_since_last_path_select))
+        time.sleep(3.2)
 
         # checking if there was a path select message in the meantime
         if self.received_since_last_path_select > 0:
-            print("received a path select message in the meantime, not sending fake server response")
             return
-        else:
-            print("sending fake server response")
-            self.callback('pathSelect', {'startDirection': startDirection})
-            print(str(startDirection) + " " + str(type(startDirection)))
 
+        # faking callback
+        self.callback('pathSelect', {'startDirection': startDirection})
