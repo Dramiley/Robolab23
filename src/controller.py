ac38aefa81d176adb7ce816ba29c486ac1c8d964
Dominik <dominik.ziegenhagel@gmail.com>
2023-03-18 13:27:36 +0100

    chang eplajn

--- src/controller.py
+++ src/controller.py
@@ -53,7 +53,7 @@ class Controller:
         # setup error handling
         self.communication.set_callback('error', lambda message: print("COMM. FEHLER GEMELDET: " + message))
 
-        test_planet = "Chadwick"
+        test_planet = "Conway"
         self.communication.test_planet(test_planet)
 
         from robot import Robot
