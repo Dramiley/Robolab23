268df02e329f2edb91863e1d64631db58662f400
Alexander Kusnezoff <alexander.kusnezoff@mailbox.tu-dresden.de>
2023-03-19 14:35:13 +0100

    :boom: ?

--- src/controller.py
+++ src/controller.py
@@ -95,7 +95,7 @@ class Controller:
 
         # let robot check paths on the node he is on and register it in planet.unexplored
         current_node = (self.last_position.x, self.last_position.y)
-        if not (current_node in self.paths.keys() and current_node not in self.unexplored_nodes):
+        if not (current_node in self.planet.paths.keys() and current_node not in self.unexplored_nodes):
             # ->we havent scanned that node yet
             self.__check_explorable_paths()
         # pdb.set_trace()
